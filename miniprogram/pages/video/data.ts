add133b39a3bc529ad0e0159fe23db1f71d87a18
hexiekuaile <fy126126@qq.com>
2020-06-12 16:36:46 +0800

    测试网上拉video信息

--- miniprogram/pages/video/data.ts
+++ miniprogram/pages/video/data.ts
@@ -4,7 +4,6 @@ export class Video {
     src: string = '';
     title: string;
     constructor(id: number, vid: string, title: string) {
-        console.log(999)
         this.id = id;
         this.vid = vid;
         this.title = title;
@@ -15,7 +14,7 @@ export class Video {
         wx.request({
             url: 'https://vv.video.qq.com/getinfo',
             data: {
-                vids: this.vid,
+                vids: that.vid,
                 platform: 101001,
                 charge: 0,
                 otype: 'json'
@@ -25,7 +24,7 @@ export class Video {
             },
             method: 'GET',
             dataType: 'json',
-            success: function (res) {
+            success(res) {
                 let str: string = res.data as string;
                 //用正则表达式
                 let url = str.match(/(?<=\[{"url":").*?(?=","vt")/);
@@ -38,6 +37,9 @@ export class Video {
                 let src = urll + fnn + "?vkey=" + fvkeyy;
                 that.src = src;
                 console.log(that.id + ' ' + that.title + ' ' + src)
+            },
+            fail(res) {
+                console.log(res.errMsg)
             }
         });
     }
@@ -47,9 +49,7 @@ export class Video {
 export let videos: Video[] = [
     new Video(1, 'x0961yzsbpk', '生态环境部宣传片'),
     new Video(2, 'z0961nhl5vi', '环境监测设施'),
-    new Video(3, 'f0979palcaj', '城市污水处理设施'),
+    new Video(3, 'n0980b7gjef', '城市污水处理设施'),
     new Video(4, 'u096134l8rp', '生活垃圾处理设施'),
     new Video(5, 'x09614l32ux', '废弃电器电子产品处理设施')
 ];
-//module.exports = { videos: videos };
-//export = videos;
--- miniprogram/pages/video/video.ts
+++ miniprogram/pages/video/video.ts
@@ -1,5 +1,6 @@
 //var externalData = require("data.js");
 import file1 = require("./data");
+import file2 = require('./data2')
 //import file2 = require("./videoURL");
 Page({
 
@@ -7,48 +8,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-    videos: file1.videos,
-    videos02: file1.videos,
-    showLoadingStatus: true,
-    timer: 0
+    //videos: file1.videos,
+    // videos02: file1.videos,
+    showLoadingStatus: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
+    console.log('onLoad')
     //this.getVideosURL();
-    var that = this
-    let t = setInterval(() => {
-      let i = that.data.videos.length;
-      file1.videos.forEach((v) => {
-        if (v.src.length > 10) i--;
-      })
-      if (i == 0) {
-        that.setData({
-          showLoadingStatus: false,
-          videos: file1.videos
-        });
-        clearInterval(that.data.timer)
-      }
-      console.log('定时器')
-    }, 100);
+    let that = this
     that.setData({
-      timer: t
-    })
-    console.log('onload')
-    /* 
-        do {
-          let i = that.data.videos.length;
-          that.data.videos.forEach((v) => {
-            if (v.src.length > 10) i--;
-          })
-          if (i == 0) this.setData({
-            showLoadingStatus: false,
-            videos: that.data.videos
-          });
-          console.log(this.data.showLoadingStatus)
-        } while (that.data.showLoadingStatus) */
+      videos: file1.videos
+    });
+    file2.getFile();
   },
 
   /**
@@ -76,78 +51,20 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload() {
-    clearInterval(this.data.timer)
+    // clearInterval(this.data.timer)
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-    return {};
-  },
-  videoErrorCallback(e: any) {
-    console.log('视频错误信息:')
-    console.log(e.detail.errMsg)
-  },
-  /**
-   * 获取几个视频的真实地址
-   */
-  /*   getVideosURL() {
-      var that = this;
-      let i = that.data.videos02.length;
-      let interfaceURL: string = "https://vv.video.qq.com/getinfo";
-  
-      that.data.videos02.forEach((v) => {
-        //this.getVideoURL(v);
-        wx.request({
-          url: interfaceURL,
-          data: {
-            vids: v.vid,
-            platform: 101001,
-            charge: 0,
-            otype: 'json'
-          },
-          header: {
-            'content-type': 'application/json;charset=UTF-8' // 默认值
-          },
-          method: 'GET',
-          dataType: 'json',
-          success: function (res) {
-            let str: string = res.data as string;
-            //用正则表达式
-            let url = str.match(/(?<=\[{"url":").*?(?=","vt")/);
-            let urll = url ? url[0] : '';
-            let fn = str.match(/(?<=,"fn":").*?(?=","fs")/);
-            let fnn = fn ? fn[0] : '';
-            let fvkey = str.match(/(?<=,"fvkey":").*?(?=","head")/);
-            let fvkeyy = fvkey ? fvkey : '';
-            //规律url + fn + '?vkey=' + fvkey 
-            let src = urll + fnn + "?vkey=" + fvkeyy;
-            v.src = src;
-            i--;
-            if (i == 0) {//表示全部获取真实地址成功
-              that.setData({
-                videos: that.data.videos02,
-                showLoadingStatus: false
-              })
-            }
-          }
-        });
-  
-      });
-    }, */
-
+    console.log('下拉刷新')
+    file1.videos.forEach((v) => {
+      v.getSrc();
+      console.log(v.src)
+    })
+    this.setData({
+      videos: file1.videos
+    })
+  }
 })
